1f6b82f9e9b4b45746a156f1ffc9134824d04391
박용주 <apexacme@gmail.com>
2022-10-06 08:51:44 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "../awscliv2.zip"
 unzip ../awscliv2.zip
-sudo .././aws/install
+sudo ./aws/install
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
